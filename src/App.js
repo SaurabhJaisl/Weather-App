f7c8159c898d76fbf7210cd795e64c1a42e64c7e
SAURABH JAISWAL <hashproxyindia@gmail.com>
2022-04-08 14:10:32 +0530

    pwa testing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import WeatherFetch from './components/WeatherFetch/WeatherFetch';
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { createRoot } from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App />);
 
 
-
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
